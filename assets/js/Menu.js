b4496d9a2776d5e4f721799fc43d1bfcb18a7c1e
Daniel Arvizu <daniel.m.arvizu@gmail.com>
2018-03-05 20:25:33 -0600

    MENU COMPLETO

--- assets/js/Menu.js
+++ assets/js/Menu.js
@@ -7,32 +7,35 @@ function statusMenu(){
     var scroll = $(window).scrollTop();
     var position1 = $('section#Home').position();
     var part1 = position1.top;
-    var part2 = part1+$('section#Home').height();
+    var part2 = part1+$('section#Home').height()-150;
 
     var x=$('section#Servicios_1').position();
     var y=x.top;
-    var part3=y+$('section#Servicios_1').height();
+    var part3=y+$('section#Servicios_1').height()-150;
     
     var a=$('section#Empresas').position();
     var b=a.top;
-    var part4=b+$('section#Empresas').height();
+    var part4=b+$('section#Empresas').height()-150;
     
     var j=$('section#proyectos').position();
     var k=j.top;
-    var part5=k+$('section#proyectos').height();
+    var part5=k+$('section#proyectos').height()-150;
     
     if (scroll>part1 && scroll<part2) {
-    	$('div#menu>div>div>ul>li>a').removeClass('active');
-    	$('div#menu>div>div>ul>li>a.home').addClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a').removeClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a.home').addClass('active');
     }else if (scroll>part2 && scroll<part3) {
-    	$('div#menu>div>div>ul>li>a').removeClass('active');
-    	$('div#menu>div>div>ul>li>a.servicios').addClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a').removeClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a.servicios').addClass('active');
     }else if(scroll>part3 && scroll<part4){
-    	$('div#menu>div>div>ul>li>a').removeClass('active');
-    	$('div#menu>div>div>ul>li>a.nosotros').addClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a').removeClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a.clientes').addClass('active');
     }else if(scroll>part4 && scroll<part5){
-    	$('div#menu>div>div>ul>li>a').removeClass('active');
-    	$('div#menu>div>div>ul>li>a.proyectos').addClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a').removeClass('active');
+    	$('div#Menu>div>div.containerOptions>ul>li>a.proyectos').addClass('active');
+    }else if( scroll>part5){
+        $('div#Menu>div>div.containerOptions>ul>li>a').removeClass('active');
+        $('div#Menu>div>div.containerOptions>ul>li>a.contactenos').addClass('active');
     }
 }
 
